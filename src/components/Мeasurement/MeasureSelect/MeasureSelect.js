6e6f3ad12396d1aca99056204fe27895ac3cc423
OlegRomashov <dead_line@list.ru>
2021-03-26 19:16:47 +0300

    fixed select component

--- src/components/Мeasurement/MeasureSelect/MeasureSelect.js
+++ src/components/Мeasurement/MeasureSelect/MeasureSelect.js
@@ -1,29 +1,29 @@
 import React from 'react'
 import './MeasureSelect.css'
 
-const sections = {
-    'Acre': 'section2',
-    'Ar': 'section3',
-    'Hectare': 'section4',
-    'SquareCentimeter': 'section5',
-    'SquareFoot': 'section6',
-    'SquareInch': 'section7',
-    'SquareMeter': 'section8'
-}
-
-const selection = function (select) {
-    for (let i in sections)
-        document.getElementById(sections[i]).style.display = "none";
-    document.getElementById(sections[select.value]).style.display = "block";
-}
-
 const MeasureSelect = () => {
+    const sections = {
+        'Acre': 'section2',
+        'Ar': 'section3',
+        'Hectare': 'section4',
+        'SquareCentimeter': 'section5',
+        'SquareFoot': 'section6',
+        'SquareInch': 'section7',
+        'SquareMeter': 'section8'
+    }
+
+    const selection = function (select) {
+        for (let i in sections)
+            document.getElementById(sections[i]).style.display = "none";
+            document.getElementById(sections[select.target.value]).style.display = "block";
+    }
+
     return (
         <div className="MeasureSelect">
             <form>
                 <div id="section1">
                     <select onChange={selection}>
-                        <option defaultValue="Acre">Акры</option>
+                        <option value="Acre">Акры</option>
                         <option value="Ar">Ар</option>
                         <option value="Hectare">Гектар</option>
                         <option value="SquareCentimeter">Квадратный сантиметр</option>
