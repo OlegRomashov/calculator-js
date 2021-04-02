fbfb056d33e7b13b93dbfc78ea83ac41635aef86
OlegRomashov <dead_line@list.ru>
2021-04-02 20:27:38 +0300

    try to do measure

--- src/components/Мeasurement/Area/Area.js
+++ src/components/Мeasurement/Area/Area.js
@@ -8,13 +8,15 @@ import './Area.css'
 class Area extends Component {
     state = {
         inputs: [
-            {name: 'inputFieldUP', value: ''},
-            {name: 'inputFieldDown', value: ''}
+            {name: 'inputFieldUP', value: 1},
+            {name: 'inputFieldDown', value: 2}
         ],
-        indexBlock: 1,
+        indexBlock: 0,
         upSelect: 'ac',
         downSelect: 'ac'
     }
+
+
 
     clearInput = () => {
         this.setState({
@@ -81,6 +83,7 @@ class Area extends Component {
 
     changeMeasureInputHandler = (index, event) => {
         const symbols = [',', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
         const value = event.target.value
         const symbol = value[value.length-1]
         if(symbols.includes(symbol)) {
@@ -90,20 +93,58 @@ class Area extends Component {
             inputs[index] = input
             this.setState({inputs})
         }
-        console.log(this.state.inputs)
+
     }
 
     changeSelectHandler = (index, select) => {
+
+        const value = select.target.value
+
         if(index === 0) {
-            this.setState({
-                upSelect: select.target.value
-            })
-            } else {
-            this.setState({
-                downSelect: select.target.value
-            })
+            const upSelect = value
+            this.setState({upSelect})
+            console.log(upSelect, this.state.upSelect)
+        } else if(index === 1) {
+            const downSelect = value
+            this.setState({downSelect})
+            console.log(downSelect, this.state.downSelect)
         }
-        console.log(index, select.target.value, this.state)
+
+        const measureTable = {
+            'ac-ac': '1', 'ac-a': '40.468', 'ac-ha': '0.404', 'ac-cm2': '40468564.224', 'ac-ft2': '43560',  'ac-in2': '6272640', 'ac-m2': '4046.856',
+            'a-ac': '0.024', 'a-a': '1', 'a-ha': '0.01', 'a-cm2': '1000000', 'a-f2': '1076.391', 'a-in2': '155000.310', 'a-m2': '100',
+            'ha-ac': '2.471', 'ha-a': '100', 'ha-ha': '1', 'ha-cm2': '100000000', 'ha-ft2': '107639.104', 'ha-in2': '15500031', 'ha-m2': '10000',
+            'cm2-ac': '2.471', 'cm2-a': '0.000001', 'cm2-ha': '0.0000001', 'cm2-cm2': '1', 'cm2-ft2': '0.001', 'cm2-in2': '0.155', 'cm2-m2': '0.0001',
+            'ft2-ac': '0.00000229', 'ft2-a': '0.000092', 'ft2-ha': '0.000009', 'ft2-cm2': '929.03', 'ft2-ft2': '1', 'ft2-in2': '144', 'ft2-m2': '0.092',
+            'in2-ac': '1.594', 'in2-a': '0.000006', 'in2-ha': '0.00000006', 'in2-cm2': '6.451', 'in2-ft2': '0.006', 'in2-in2': '1', 'in2-m2': '0.0006',
+            'm2-ac': '0.0002', 'm2-a': '0.01', 'm2-ha': '0.0001', 'm2-cm2': '10000', 'm2-ft2': '10.763', 'm2-in2': '1550.003', 'm2-m2': '1'
+        }
+        const measure = this.state.upSelect + '-' + this.state.downSelect
+        const coefficient = parseFloat(measureTable[measure])
+
+           // const {input1, input2} = this.state.inputs ниже замена
+        // const input1 = inputs[0]
+        // const input2 = inputs[1]
+        // if(index === 0) {
+        //     if(input2.value !== ''){
+        //         input2.value = input2.value * coefficient
+        //         inputs[1] = input2
+        //         this.setState({inputs})
+        //     }
+        // } else if(index === 1) {
+        //     if(input1.value !== ''){
+        //         input1.value = input1.value * coefficient
+        //         inputs[0] = input1
+        //         this.setState({inputs})
+        //     }
+        // }
+
+
+        // console.log('index:', index)
+        // console.log('value:', value)
+        // console.log('measure:', measure)
+        // console.log('type of coefficient:', coefficient)
+        // console.log('state:', this.state)
     }
 
     render() {
